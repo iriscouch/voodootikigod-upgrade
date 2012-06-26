8a5fb3230057bd26c1f903f42cbe41546d3e5d1d
Jason Smith (air) <jhs@iriscouch.com>
2012-06-26 07:30:39 +0700

    Switch to probe_couchdb, and display the domains to be fixed

--- upgrade.js
+++ upgrade.js
@@ -5,23 +5,49 @@
 var txn = require('txn')
 var util = require('util')
 var request = require('request')
+var probe_couchdb = require('probe_couchdb').defaults({'do_users':false})
 
+var FIX_DOMAINS = [ 'couchdb.minutewith.com', 'mobile.minutewith.com', 'node.minutewith.com', 'riak.minutewith.com'
+                  , 'www.aminutewithbrendan.com', 'staging.aminutewithbrendan.com' ]
 var url = process.env.iriscouch_url || process.argv[2]
 
 if(!url)
   return console.error('Usage: upgrade.js https://admin:secret@voodootikigod.iriscouch.com:6984')
 
-url = url.replace(/\/+$/, '')
 console.log('Checking: %s', url)
-request({'url':url+'/_session', 'json':true}, function(er, res) {
-  if(er)
-    throw er
+var couch = new probe_couchdb.CouchDB(url)
+couch.start()
 
-  var roles = res.body.userCtx.roles
+couch.on('error', function(er) {
+  console.error('Error: %s', er.message)
+  throw er
+})
+
+couch.on('session', function(session) {
+  var roles = session.userCtx.roles
   if(!~ roles.indexOf('_admin'))
     return console.error('Must be an admin on this couch')
-  else
-    console.log('Good couch, fixing...')
 
-  console.log('TODO')
+  console.log('Good couch, fixing...')
+  couch.known('config', fix_vhosts)
 })
+
+function fix_vhosts(config) {
+  var vhosts = []
+  Object.keys(config.vhosts).forEach(function(domain) {
+    if(!~ FIX_DOMAINS.indexOf(domain))
+      return console.log('Skipping vhost: %s', domain)
+    else
+      vhosts.push({'domain':domain, 'target':config.vhosts[domain]})
+  })
+
+  fix_domain()
+  function fix_domain() {
+    var vhost = vhosts.shift()
+    if(!vhost)
+      return console.log('Fixed all domains')
+
+    console.log('Should fix %s %s', vhost.domain, vhost.target)
+    fix_domain()
+  }
+}
