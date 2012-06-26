8d9840c03af0bbd91e56d34e71692f1411630b14
Jason Smith (air) <jhs@iriscouch.com>
2012-06-26 08:19:30 +0700

    Fix the rewrite rules for each vhost: add a "*" rule, and convert query values to string

--- upgrade.js
+++ upgrade.js
@@ -33,12 +33,20 @@ couch.on('session', function(session) {
 })
 
 function fix_vhosts(config) {
+  var couch = this
+
   var vhosts = []
   Object.keys(config.vhosts).forEach(function(domain) {
     if(!~ FIX_DOMAINS.indexOf(domain))
       return console.log('Skipping vhost: %s', domain)
-    else
-      vhosts.push({'domain':domain, 'target':config.vhosts[domain]})
+
+    var target = config.vhosts[domain]
+      , match = target.match(/^(.*)\/_rewrite$/)
+
+    if(!match)
+      throw new Error('Unknown vhost target: %s', target)
+
+    vhosts.push({'domain':domain, 'ddoc':match[1]})
   })
 
   fix_domain()
@@ -47,7 +55,60 @@ function fix_vhosts(config) {
     if(!vhost)
       return console.log('Fixed all domains')
 
-    console.log('Should fix %s %s', vhost.domain, vhost.target)
-    fix_domain()
+    var url = couch.url + '/' + vhost.ddoc
+    console.log('Fixing %s in %s', vhost.domain, vhost.ddoc)
+    return txn({'url':url}, fix_ddoc, ddoc_fixed)
+
+    function fix_ddoc(ddoc, to_txn) {
+      if(!ddoc.rewrites || !Array.isArray(ddoc.rewrites))
+        return to_txn(new Error('Bad rewrites in ddoc: ' + vhost.ddoc))
+
+      //console.log('Rewrite rules for %s:\n%s', vhost.ddoc, util.inspect(ddoc.rewrites))
+
+      var rewrites = []
+        , blank_rewrite = null
+        , wildcard_rewrite = null
+
+      ddoc.rewrites.forEach(function(rewrite) {
+        if(rewrite.from == '*')
+          return console.log('Ignore wildcard rewrite: %j', rewrite)
+
+        if(rewrite.from == '') {
+          blank_rewrite = rewrite
+
+          wildcard_rewrite = JSON.parse(JSON.stringify(blank_rewrite))
+          wildcard_rewrite.from = '*'
+        }
+
+        if(rewrite.query)
+          Object.keys(rewrite.query).forEach(function(key) {
+            var val = rewrite.query[key]
+            if(typeof val == 'boolean' || typeof val == 'number')
+              rewrite.query[key] = "" + val
+          })
+
+        rewrites.push(rewrite)
+      })
+
+      if(!blank_rewrite)
+        return to_txn(new Error('Cannot find empty string rewrite, "": ' + vhost.ddoc))
+      if(!wildcard_rewrite)
+        return to_txn(new Error('Failed to build wildcard rewrite, "*": ' + vhost.ddoc))
+
+      rewrites.push(wildcard_rewrite)
+
+      ddoc.rewrites = rewrites
+      //console.log('New rewrite rules for %s:\n%s', vhost.ddoc, util.inspect(ddoc.rewrites))
+
+      return to_txn()
+    }
+
+    function ddoc_fixed(er) {
+      if(er)
+        throw er
+
+      console.log('Fixed: %s', vhost.domain)
+      fix_domain() // Do the next one (serially).
+    }
   }
 }
